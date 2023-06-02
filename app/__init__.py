d9735ba4d2b360ad3f9d60f3038663961995a942
DelReA <delos.reyes.alvin@outlook.com>
2023-06-02 17:05:04 +0200

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from config import Config
+from app.extensions import db
+
+
+def create_app(config_class=None):
+  
+  if config_class is None:
+    config_class = Config
+
+  app = Flask(__name__)
+  app.config.from_object(Config)
+
+  # Initialize Flask extensions here
+  db.init_app(app)
+
+  return app
